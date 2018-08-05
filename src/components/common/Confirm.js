d3a063eeb8f0b085203635c48a66c568b2fcb111
tingsyuanwang <dtvgood202@gmail.com>
2018-08-05 15:02:18 +0800

    Styling the Modal

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -4,16 +4,20 @@ import { CardSection } from "./CardSection";
 import { Button } from "./Button";
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+    const { containerStyle, textStyle, cardSectionStyle } = styles;
+
     return (
         <Modal
             visible={visible}
             transparent
             animationType="slide"
-            onRequestCole{() => {}}
+            onRequestClose={() => {}}
         >
-            <View>
-                <CardSection>
-                    <Text>{children}</Text>
+            <View style={containerStyle}>
+                <CardSection style={cardSectionStyle}>
+                    <Text style={textStyle}>
+                        {children}
+                    </Text>
                 </CardSection>
                 <CardSection>
                     <Button onPress={onAccept}>Yes</Button>
@@ -22,6 +26,25 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
             </View>
         </Modal>
     );
+};
+
+const styles = {
+    cardSectionStyle: {
+        justifyContent: 'center',
+    },
+    textStyle: {
+        flex: 1,
+        fontSize: 18,
+        textAlign: 'center',
+        lineHeight: 40
+    },
+    containerStyle: {
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        position: 'relative',
+        flex: 1,
+        justifyContent: 'center',
+
+    }
 };
 
 export { Confirm };
